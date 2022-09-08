2fbf0e0fc7cebcb380ad221a17909d4e238f65ee
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-08 20:57:42 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+* main()- program
+* return is always 0
+**/
 int main(void)
-    {
-      printf("with proper grammar, but the outcome is a piece of are,\n");
-      return (0);
-    }
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
