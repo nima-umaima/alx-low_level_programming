4fd0658d1e11992aca088fbfa4c96d253d17e380
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-08 21:18:10 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
