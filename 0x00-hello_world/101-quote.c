78ae34496d593707d0bb5b116307432673d8e39d
nima-umaima <naimanysh@gmail.com>
2022-09-08 11:13:42 -0700

    5-5

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return - Always 0 (Success)
+ */
+
+main (void){
+  write(2, "and that piece of art is useful\" - Dora Korpor, 2015-10-19\n" , 61);
+  return (1);
+}
