08e082ec9852b2e291df8ec5ce7bb584c9787ba7
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-08 21:05:58 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
-int main(){
-  puts("\"Programing is like building a multilingual puzzle);
-                 return (0);
-   }
+/**
+ * main - Short description, single line
+ * Description: Longer description of the function)?
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
