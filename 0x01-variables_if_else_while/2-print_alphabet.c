bce620a427176f9bb2ffe8e6b09449ca23d6ec19
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-09 21:02:19 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,6 @@ int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
