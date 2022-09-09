c61bb606144e18a0f0d9deb3fcaadc49ee711959
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-09 22:04:53 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * main - Prints the alphabet without q and e.
- * Return: Always 0 (Success)
- */
+* main - Prints the alphabet without q and e.
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i;
