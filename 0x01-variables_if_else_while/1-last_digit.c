81a6db3b60a0ae3a8c45fdb1afca3edc8ef44b3b
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-09 20:38:36 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int n;
 	char last[] = "Last digit of";
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-  /* your code goes there */
 	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
