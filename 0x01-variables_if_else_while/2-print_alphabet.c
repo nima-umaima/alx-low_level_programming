644c67b16ab2c7105e81d304ce2bb917bfedac7c
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-09 21:00:12 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabet.
  *
- *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char c;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (c = 'a' ; c <= 'z' ; c++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar(c);
+		putchar(alp[i]);
 	}
 	putchar('\n');
+	return (0);
 }
