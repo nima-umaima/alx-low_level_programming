543dc48941f0cc28ce0a31f646386fb5a2aa8564
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-09 22:13:05 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/**
- * main - Entry point
-(*
- * Return: 0 on success
- */
+#include <stdlib.h>
+#include <string.h>
+
+/** 
+* Main - Prints the alphabet without q and e
+*
+*Return 0 (success)
+*/
 int main(void)
 {
-	int n;
-  for (i = 97; i < 123; i++)
-  if (i != 101 && i != 113)
-  putchar(i);
-  putchar('\n');
-  return (0);
+    int i;
+    for (i =97; i < 123; i++)
+    {
+       if (i != 101 && i != 113)
+       {
+          putchar(i);
+       }
+    }
+    putchar('\n');
+    rerurn (0);
 }
