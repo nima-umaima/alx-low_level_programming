6f84dbc07bf0e5796980b6e61963d3dae5b8f2af
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-13 15:47:36 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  *@c: letter being tested
  * Return: 1(Success) 0(Not Success)
  */
-int _isalpha(inc c)
+int _isalpha(int c)
 {
   if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
     return (1);
