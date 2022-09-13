a04b7f8f94a71f0daef53913b645c258c5adcfa7
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-13 15:07:54 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 {
 int c2;
 int c;
-for(c2 = 0; c2 <= 9; c2++)
+for (c2 = 0; c2 <= 9; c2++)
 {
- for(c = 'a'; c <= 'z'; c++)
-    {
+for (c = 'a'; c <= 'z'; c++)
+{
 	_putchar(c);
-    }
-    _putchar('\n');
-   }
+	}
+	_putchar('\n');
+	}
 }
