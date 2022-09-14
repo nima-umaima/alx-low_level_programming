6765b8214bb79f082918591ddf3084ccff292a77
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-14 20:19:46 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,15 +5,17 @@
  *@n: starting point
  *  Return: ALways 0
  */
-void print_to_98()
+void print_to_98(int n)
 {
-  if (n <= 98)
-    {
-      for (n = n; n <= 97; n++)
-	printf("%d, ", n);
-    }
-  else
-    for (n = n; n > 98; n--)
-      printf("%d, ", n);
-  printf("98\n");
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+	}
+	else
+	{
+		    for (n = n; n > 98; n--)
+			    printf("%d, ", n);
+	}
+	printf("98\n");
 }
