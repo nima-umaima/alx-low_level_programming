45a9b78a2a7e6b941627d8e22b04fd2e42e376f4
nima-umaima <naimanysh@gmail.com>
2022-09-14 09:18:40 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - Compute the absolute value of an integer
+ *@n: Number being tested
+ * Return: Always 0
+ */
+int _abt(int n)
+{
+  if(n < 0)
+    {
+      return (-n);
+    }
+  else
+    {
+      return (n);
+    }
+  return (0);
+}
