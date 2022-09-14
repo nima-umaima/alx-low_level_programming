3af3f61413130a30c24e20cdc84e4837942657e5
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-14 19:22:08 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,15 +5,15 @@
  *@n: Number being tested
  * Return: Always 0
  */
-int _abt(int n)
+int _abs(int n)
 {
-  if(n < 0)
-    {
-      return (-n);
-    }
-  else
-    {
-      return (n);
-    }
-  return (0);
+if(n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+return (0);
 }
