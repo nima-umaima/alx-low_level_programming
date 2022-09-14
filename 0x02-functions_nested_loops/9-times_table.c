8f4c2b792073c12d921da4733b3317fc2c78c7f2
nima-umaima <naimanysh@gmail.com>
2022-09-14 11:50:21 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * times_table - prints the times table up to 9
+ * Return: Always 0
+ */
+void main(void)
+{
+  int a, b, c, d, e;
+  for (a = 0; a < 10; a++)
+    {
+      for (b = 0; b < 10; b++)
+	{
+	  c = a * b;
+	  d = c / 10;
+	  e = c % 10;
+	  if (b == 0)
+	    _putchar('0');
+	  else if (c < 10)
+	    {
+	      _putchar('0' + d);
+	      _putchar('0' + e);
+	    }
+	  if (b < 9)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	    }
+	  else
+	    _putchar('\n');
+	}
+    }
+}
