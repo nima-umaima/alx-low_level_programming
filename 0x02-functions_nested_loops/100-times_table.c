d4d84744b8dde012f48952cfbba2b822e581d6fb
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-14 20:50:17 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,36 +12,37 @@ if (n >= 0 && n < 15)
 {
 for (a = 0; a <=  n; a++)
 {
-  for (b = 0; b <= n; b++)
-  {
-    c = a * b; d = c / 100; e = (c /  10) % 10;
-    f = (c % 100) % 10;
-    if (b == 0)
-    {
-      _putchar('0');
-    }
-    else if (c < 10)
-    {
-      _putchar(' '); _putchar(' ');
-      _putchar('0' + f);
-    }
-    else if (c < 100)
-    {_putchar(' ');
-     _putchar('0' + e); _putchar('0' + f);
-    }
-    else
-    {
-      _putchar('0' + d);
-      _putchar('0' + e);
-      _putchar('0' + f);
-    }
-    if (b < n)
-    {
-      _putchar(','); _putchar(' ');
-    }
-    else
-      _putchar('\n');
-  }
+for (b = 0; b <= n; b++)
+{
+c = a * b; d = c / 100; e = (c /  10) % 10;
+f = (c % 100) % 10;
+if (b == 0)
+{
+_putchar('0');
+}
+else if (c < 10)
+{
+_putchar(' '); _putchar(' ');
+_putchar('0' + f);
+}
+else if (c < 100)
+{_putchar(' ');
+ _putchar('0' + e);
+ _putchar('0' + f);
+}
+else
+{
+_putchar('0' + d);
+_putchar('0' + e);
+  _putchar('0' + f);
+}
+if (b < n)
+{
+  _putchar(','); _putchar(' ');
+}
+else
+_putchar('\n');
+}
 }
 }
 }
