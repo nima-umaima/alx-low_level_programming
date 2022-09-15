32ad5011b06fd8750c2f02ecfe8cc4bf8afa913c
nima-umaima <naimanysh@gmail.com>
2022-09-15 11:38:23 -0700

    1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - causes and infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+  int i;
+  printf("infinit loop incomming :(''\n);
+
+i = 0;
+
+while (i < 10)
+{
+putchar(i)
+}
+printf("inifinite loop avoided! \\o/n");
+
+return (0);
+}
