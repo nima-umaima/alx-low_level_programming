54ed401641abec5080d074e5b15e140568d4e09a
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-15 21:53:17 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,16 +8,17 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
-/**
-* while - loop start
+/*
 *  while (i < 10)
 * {
 * putchar(i);
 *  }
-* \\o/\n: loop avoided
 */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
