9d23015b3b83cfc9416f964a47185a8c76e21137
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-15 19:28:27 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,14 +6,14 @@
  */
 void pritn_square(int size)
 {
-  int x;
-  int y;
-  for (y = 0; y < size; y++)
-    {
-  for (x = 0; x < size; x++)
-    _putchar('#');
-  _putchar('\n');
-    }
-  if (size < 1)
-    _putchar('\n');
-}
+ int x;
+ int y;
+ for (y = 0; y < size; y++)
+ {
+ for (x = 0; x < size; x++)
+ _putchar('#');
+ _putchar('\n');
+ }
+ if (size < 1)
+ _putchar('\n');
+ }
