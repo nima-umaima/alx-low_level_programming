04ad595facb9e667ba179ff22f237ea3aa23af56
nima-umaima <naimanysh@gmail.com>
2022-09-15 02:35:38 -0700

    file

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _putchar - write the character c to stdout
+ *@c: The character to print
+ * Return: Always 0
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
