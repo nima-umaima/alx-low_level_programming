0ff7e26e52a8457ad9913ce51dd66cf204aae5e2
nima-umaima <naimanysh@gmail.com>
2022-09-15 01:34:00 -0700

    0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check for uppercase characters
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+  if (c >- 65 && c <= 90)
+    return (1);
+  else
+    return (0);
+}
