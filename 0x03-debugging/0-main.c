f700c3a13c6ee5dfe175024e4d0ccaf124ad83a9
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-15 21:33:53 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,11 +3,14 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-int main(void)
+void positive_or_negative(int i)
+{ 
+if (i < 0)
 {
-  int i;
-  
-  i=0;
-  positive_or_negative(i);
-  return (0); 
+return (-i);
+}
+else
+{
+return (i);
+}    
 }
