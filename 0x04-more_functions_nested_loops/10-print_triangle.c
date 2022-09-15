d8b6e86f2e214cf333f5c7cad7b4574fdc959568
nima-umaima <naimanysh@gmail.com>
2022-09-15 06:33:48 -0700

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print triangle
+ * Return: Always 0
+ */
+void print_triangle(void)
+{
+  int x;
+  int y;
+  for ()
+    {
+      for ()
+	{
+	  if ()
+	    _putchar();
+	  else
+	    _putchar();
+	}
+      _putchar('\n');
+    }
+}
