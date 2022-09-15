c9c9ff80b547217f1eb38ab93f5ef28e66d3156f
nima-umaima <naimanysh@gmail.com>
2022-09-15 10:58:37 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,9 @@
+#include <main.h>
+/**
+ * positive_or_negative - test wheather integer is positive or negative
+ *@i: the integer to test
+ * Return: Always 0
+ */
+void positive_or_negative(int i)
+{
+}
