c51431cce070763764f85f2e0bc27ce41e0ba6ee
nima-umaima <naimanysh@gmail.com>
2022-09-15 10:11:16 -0700

    12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - takes an integer and prints it with _putchar
+ *@n: integer to print
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+  int ;
+  int ;
+  a0 = n;
+  a2 =n;
+  a3 = n;
+  if ()
+    {
+      _putchar('-');
+    }
+  if ()
+    _putchar();
+  if ()
+    _putchar()
+      }
