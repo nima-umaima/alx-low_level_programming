26e92d2f074e68c860e5a7d407ff9533d4ff59fd
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-15 19:17:52 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,14 +6,14 @@
  */
 void print_diagonal(int n)
 {
-  int x, y;
-  for (y = 1; y <= n; y ++)
-    {
-  for (x = 1; x < y; x++)
-    _putchar(' ');
-  _putchar('\\');
-  _putchar('\n');
+int x, y;
+for (y = 1; y <= n; y ++)
+{
+for (x = 1; x < y; x++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
 }
 if (n < 1)
-  _putchar('\n');
+_putchar('\n');
 }
