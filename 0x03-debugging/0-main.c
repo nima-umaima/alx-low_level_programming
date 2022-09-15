32e95c0dfbb83f80cdfbb2c505cbf9be45c35bf6
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-15 21:07:58 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,13 @@
 #include <main.h>
 /**
- * positive_or_negative - test wheather integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  *@i: the integer to test
  * Return: Always 0
  */
-void positive_or_negative(int i)
+int main(void)
 {
+int i;
+i = 0;
+positive_or_negative(i);
+return (0); 
 }
