e2062d76467e39cc7856ee1c9180deefdb150d31
nima-umaima <naimanysh@gmail.com>
2022-09-15 02:15:49 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Multiply 2 numbers
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+  printf("%d\n", mul(98, 1024));
+  printf("%d\n", mul(-402, 4096));
+  return (0);
+}
