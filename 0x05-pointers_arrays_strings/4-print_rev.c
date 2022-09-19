1e17e7b105713b1ed9fbaa55e9f8aedfe03c6736
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-19 22:29:55 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,12 +3,12 @@
  * print_rev - Prints a string in reverse.
  *@s: strint to be reversed
  */
-void print_rev(char *s)
+void print_rev(char *str)
 {
 int len = 0, i;
-while (s[i++])
+while (str[i++])
 len++;
 for (i = len - 1; i >= 0; i--)
-_putchar(s[i]);
+_putchar(str[i]);
 _putchar('\n');
 }
