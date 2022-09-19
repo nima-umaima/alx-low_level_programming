9d3a31694d906add1d365635fdc7b52c5622f89e
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-19 22:17:20 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@ int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
-void _puts(cahr *str);
+void _puts(char *str);
 void print_rev(char *s);
 
 #endif
