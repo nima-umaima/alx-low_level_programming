2c9625955e28cc82acb6408bd3b7b1b645dff2b3
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-19 23:11:19 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,10 @@ void puts2(char *str)
 int n = 0;
 for (; str[n] != '\0'; n++)
 {
-  if ((n % 2) == 0)
+if ((n % 2) == 0)
 _putchar(str[n]);
 else
 continue;
 }
- _putchar('\n');
+_putchar('\n');
 }
