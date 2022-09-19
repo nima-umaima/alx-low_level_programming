1781df2a437dd65dd0727a9de56af0033f197825
nima-umaima <naimanysh@gmail.com>
2022-09-19 13:48:53 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mian - check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+  char s1[98];
+  char *ptr;
+  ptr = _strcpy(s1, "Fisrt, solve the problem. Then, write the code\n");
+  printf("%s", s1);
+  printf("%s", ptr);
+  return (0);}
