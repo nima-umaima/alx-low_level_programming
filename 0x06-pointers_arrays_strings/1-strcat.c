7aa97619374fba35ac0204d5cd81c2d2eeb80782
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-22 12:54:28 +0300

    Update 1-strcat.c

---
+++ 0x06-pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat - concatinates two strings
+ *@dest: string to append by src
+ *@src: string to append to dest
+ *@n: largest number of bytwss to append
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+i = j = 0;
+while (*(dest + i))
+i++;
+while (j < n && *(src + j))
+{
+*(dest + i) = *(src + j);
+i++;
+j++;
+}
+return (dest);
+}
