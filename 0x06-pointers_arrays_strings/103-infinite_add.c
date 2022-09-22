ec419304e951b89ba8648f28d70607558a2c8d74
nima-umaima <naimanysh@gmail.com>
2022-09-22 05:15:55 -0700

    11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * infinite_add - adds two numbers
+ *@n1: first number
+ *@n2: second number
+ *@r: buffer for result
+ *size_r: buffer size
+ * Return: address of r or 0
+ */
+char
+{
+int;
+for ()
+for ()
+if ()
+return (0);
+m = 0;
+for ()
+{
+n = m;
+if (i >= 0)
+  n += n1
+    if
+      }
+}
