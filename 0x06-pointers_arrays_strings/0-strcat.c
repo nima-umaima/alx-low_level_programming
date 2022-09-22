acbda57cc162ecb41750a3ebef0f1ce5c0879945
nima-umaima <naimanysh@gmail.com>
2022-09-22 02:01:00 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#inlcude "main.h"
+/**
+ * _strcat - concatinates two strings
+ *@dest: string to append to src
+ *@src: string to appendby dest
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = j = 0;
+while (*(dest + 1))
+i++;
+while ((*(dest + i) = *(src + j)))
+{
+i++;
+j+}
+return (dest);
+}
