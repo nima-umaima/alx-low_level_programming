5e19b04364df890a1ff2da5825b9be17d1c3c53a
nima-umaima <naimanysh@gmail.com>
2022-09-22 04:46:52 -0700

    9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_numbers - prints integer
+ *@n: integer printed
+ * Return: void
+ */
+void print_number(int n)
+{
+  int ;
+  int ;
+  a0 = n;
+  a2 = ;
+  a3;
+  a4;
+  a5;
+  a6;
+  if (n < 0)
+    {
+      _putchar('-');
+      a0;
+      a5;
+    }
+  if ()
+    _putchar();
+  if();
+}
