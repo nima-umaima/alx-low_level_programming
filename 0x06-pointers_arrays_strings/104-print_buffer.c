562093f607acbfea22a83c4357c202914da287b8
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-22 15:30:40 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,10 +2,26 @@
 #include <stdio.h>
 /**
  * print_buffer - prints a buffer
- *@b
- *@size
- * Return: 
+ *@b: buffer to print
+ *@size: bytes of the buffer pointed by b
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
 }
