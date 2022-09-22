92900249f69c2c8b410bdaac745615e9a8da8281
nima-umaima <naimanysh@gmail.com>
2022-09-22 04:01:14 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#inlcude "main.h"
+/**
+ * string - changes lower case to upper case
+ *@s: string to change
+ * Return: address of s
+ */
+char *string_toupper(char *)
+{
+int i = 0;
+while (*(s + i))
+{
+if (*(s + i) >= 'a' && *(s + i) <= 'z')
+*(s + i) -= 'a' - 'A';
+i++;
+}
+return (s);
+}
