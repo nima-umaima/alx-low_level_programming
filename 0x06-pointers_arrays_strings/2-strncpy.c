cd8bfa28620cc161994f6b0f6f86ea8b442087ae
nima-umaima <naimanysh@gmail.com>
2022-09-22 03:01:10 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,12 @@
+#inlcude "main.h"
+/**
+ * _strncopy - copies a string
+ *@dest: string to copy by src
+ *@src: string to copy to
+ *@n:
+ * Return: dest
+ */
+char *_strncpy()
+{
+  return (dest);
+}
