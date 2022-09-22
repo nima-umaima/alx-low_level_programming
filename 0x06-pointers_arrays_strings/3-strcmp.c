9fa4af15f84b33d97d2aed14dbc4115ed35d20ac
nima-umaima <naimanysh@gmail.com>
2022-09-22 03:35:36 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#inlcude "main.h"
+/**
+ * _strcmp - compares two strings
+ *@s1: first string
+ *@s2: second string
+ * Return: the difference between s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while ()
+i++;
+return();}
