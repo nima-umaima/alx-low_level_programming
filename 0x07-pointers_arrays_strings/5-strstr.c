2e34b18b5415cb5e8d6e706bacd52f939da8240c
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 20:38:31 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,23 @@
 #include "main.h"
 /**
  * _strstr - locates a substring
- *@haystack:
- *@needle:
- * Return: 
+ *@haystack: occurance to find
+ *@needle:string to search
+ * Return: pointer
  */
-char()
+char *_strstr(char *haystack, char *needle)
 {
+while (*haystack)
+{
+char *src = haystack;
+char *sub = needle;
+while (*haystack && *sub && *haystack == *sub)
+{
+haystack++;
+sub++;
 }
+if (!*sub)
+return (src);
+haystack = src + 1;
+}
+return (0);
