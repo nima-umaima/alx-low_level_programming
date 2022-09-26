a12be2494ba3443381106875c221663049df23c4
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 20:42:58 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,16 @@
 #inlude "main.h"
 /**
  * print_chessboard - prints the chessboard
- *@a:
- * Return: 
+ *@a: double pointer
+ * Return: 0
  */
-char()
+void print_chessboard(char (*a)[8])
 {
+unsigned int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+_putchar(a[i][j]);
+_putchar('\n');
+}
 }
