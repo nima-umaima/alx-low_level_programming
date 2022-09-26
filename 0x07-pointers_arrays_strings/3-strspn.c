999897aa07b8c9842153f49ee364d3bc9f8dd062
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 22:19:13 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,19 +7,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int count;
-while (*s != '\0')
+int j;
+unsigned int count = 0;
+for (; *s != '\0'; s++)
 {
-while (*accept != '\0')
+j = 0;
+while (accept[j] != '\0')
 {
-if (*s == *accept)
-break;
-count++;
-accept++;
-s++;
-}
-if (*accept == '\0')
+if (*s == accept[j])
+{
+++count;
 break;
 }
-return (count + 1);
+j++;
+}
+if (accept[j] == '\0')
+{
+return (count);
+}
+return (count);
 }
