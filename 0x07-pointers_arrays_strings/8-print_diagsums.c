7b4b4e6377f79e9d82485fd563c2113f22e274e7
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 21:03:02 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - prints sum of the two diagnals of a square matrix of integers
- *@a:
- *@size:
- * Return: 
+ *@a: pointer to an array
+ *@size: size of the matrix
+ * Return: void
  */
-char()
+void print_diagsums(int *a, int size)
 {
+int i, j;
+int c = 0;
+int d = 0;
+for (i = 0; i < size; i++)
+{
+c = c + *(a + i * sizeof(int));
+}
+for (j = 0; j < size; j++)
+{
+d = d + *(a + (size * j) + (size - j - 1));
+}
+printf("%d, %d\n", c, d);
 }
