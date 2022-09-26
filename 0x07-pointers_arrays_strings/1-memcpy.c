bc8152ee14a18505f9d68cfa75c76678a31eaea9
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 19:51:46 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,10 +3,15 @@
  * _memcpy - copies memory area
  *@dest: memory to copy to
  *@src: memory to copy form
- *@n:
+ *@n: content in byte to copy
  * Return: 
  */
-char()
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  int;
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(dest + i) = *(src + i);
+}
+return (dest);
 }
