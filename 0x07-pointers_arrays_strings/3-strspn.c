cb5da78f534e3e1da69da1926d3236ca253629e5
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 20:23:54 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strspn - gets the length of a prefix substring
- *@s:
- *@accept:
- * Return: 
+ *@s: character to print
+ *@accept: character
+ * Return: i
  */
-char()
+unsigned int _strspn(char *s, char *accept)
 {
-  int;
-  while ()
-    {}
+int count;
+while (*s != '\0')
+{
+while (*accept != '\0')
+{
+if (*s == *accept)
+break;
+count++;
+accept++
+s++;
+}
+if (*accept == '\0')
+break;
+}
+return (count + 1);
 }
