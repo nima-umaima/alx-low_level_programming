6e9640323a12563f32571c2861294ab3a4036984
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 21:30:06 +0300

    Update _putchar.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,1 +1,10 @@
-#
+#include <unistd.h>
+/**
+* _putchar - writes the character c to std out.
+*@c: the character to print
+* Return: 1 on success, -1 error
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
