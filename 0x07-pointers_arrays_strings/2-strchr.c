6b6b2d87c7fcf0506486fe0892ce4fe06516ae9b
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-26 20:14:53 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _strchr - locates a character in a string
- *@s:
- *@c:
- * Return:
-char()
+ *@s: string to check
+ *@c: character to find
+ * Return: pointer to s or null
+ */
+char *_strchr(char *s, char c)
 {
-while ()
+while (*s)
 {
-if ()
+s++;
+if (*s == c)
 {
-}}
-return ();
+return (s)
+}
+}
+return (0);
 }
