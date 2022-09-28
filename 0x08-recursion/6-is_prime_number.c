80ebb4dab4c7688ae685770a75615bd99b6dde7b
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-28 21:17:57 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,1 +1,32 @@
-#inlcude ""
+#include "main.h"
+/**
+* check_prime - num
+* @n: number
+* @c: check if is prime
+* Return: check_prime
+*/
+int check_prime(int n, int c)
+{
+if (n <= 1)
+{
+return (0);
+}
+if (n % c == 0)
+{
+return (0);
+}
+if (c == n / 2)
+{
+return (1);
+}
+return (check_prime(n, c + 1));
+}
+/**
+* is_prime_number - check if a num is prime
+* @n: number
+* Return: is_prime_number
+*/
+int is_prime_number(int n)
+{
+return (check_prime(n, 2));
+}
