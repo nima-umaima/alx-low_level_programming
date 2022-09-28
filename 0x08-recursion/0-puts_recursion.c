a32bee4dd7553f4589a6ed7c2807e9278f7ed322
nima-umaima <naimanysh@gmail.com>
2022-09-28 10:50:20 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - create put fun
+ *@s: string
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+  if ()
+    {
+      _putchar('\n');
+    }
+  else
+    {
+      _putchar(*s);
+      _putchar(s + 1);
+    }
+}
