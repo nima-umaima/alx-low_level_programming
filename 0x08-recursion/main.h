1bb5a3213a82d5c04758efc557b0ad948e0cfb8f
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-28 21:39:01 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,3 +10,4 @@ int is_prime_number(int n);
 int _putchar(char c);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+#endif
