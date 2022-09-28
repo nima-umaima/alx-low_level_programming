693b0b13cdebab12795b27b64ea77c0c59a8ce05
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-28 21:07:37 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,22 @@
 #inlcude "main.h"
 /**
- * fun -
- *@x:
- *@y:
- * Return:
+ * _pow_recursion - pow
+ *@x: number
+ *@y: power
+ * Return: x power of y
  */
-int()
+int _pow_recursion(int x, int y)
 {
+if (y < 0)
+{
+return (-1);
+}
+if (y == 0)
+{
+return (1);
+}
+else
+{
+return (_pow_recursion(x, y - 1) * x);
+}
 }
