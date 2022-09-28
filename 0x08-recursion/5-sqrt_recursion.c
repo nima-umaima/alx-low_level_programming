20064f2d17352c926d10dc164ba8cadcb5b64444
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-28 21:13:35 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,36 @@
 #include "main.h"
 /**
- * 
- *@n:
- * Return:
+ * check_sqrt - check square root
+ *@n: number
+ *@root: square root
+ * Return: check_sqrt
  */
-int()
+int check_sqrt(int n, int root)
 {
+if (root * root == n)
+{
+return (root);
+}
+if (root == n / 2)
+{
+return (-1);
+}
+return (check_sqrt(n, root + 1));
+}
+/**
+* _sqrt_recursion - square root
+* @n: number
+* Return: square root of n
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 1)
+{
+return (1);
+}
+return (check_sqrt(n, 0));
 }
