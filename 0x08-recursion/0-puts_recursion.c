644d647bdc9c5b32a03f2573eb049edcfb9d4d2b
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-28 20:52:22 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-  if ()
-    {
-      _putchar('\n');
-    }
-  else
-    {
-      _putchar(*s);
-      _putchar(s + 1);
-    }
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
 }
