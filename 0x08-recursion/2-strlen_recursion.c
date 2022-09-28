3219422e8f649f3328f02c960f7fe35c9f7b0cb7
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-28 20:59:34 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,17 @@
 # include "main.h"
 /**
- * funioaj
- *@s:
- * Return:
+ * _strlen_recursion - returns the length of a string.
+ *@s: string
+ * Return: length
  */
-void()
+int _strlen_recursion(char *s)
 {
+if (*s == '\0')
+{
+return (0);
+}
+else
+{
+rreturn (1 + _strlen_recursion(s + 1));
+}
 }
