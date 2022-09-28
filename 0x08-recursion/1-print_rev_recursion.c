dcc0b6d7e19a78ca31000fdf5d83d74bbdc586ab
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-28 20:56:00 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,14 @@
 #include "main.h"
 /**
- * print_rev_recurstion
- *@s:
- * Return: 
+ * _print_rev_recursion - reverse string
+ *@s: string
+ * Return: s
  */
-void()
+void _print_rev_recursion(char *s)
 {
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
 }
