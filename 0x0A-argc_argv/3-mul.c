ddfdf71f3c54b690607c0903e79d4e657e53e7b5
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-30 20:48:30 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,1 +1,24 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*
+*
+*
+*
+*/
+int main(int argc, char *argv[])
+{
+int m = 0;
+if (argc == 3)
+{
+m = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", m);
+return (0);
+}
+else
+{
+printf("Error\n");
+}
+return (1);
+}
