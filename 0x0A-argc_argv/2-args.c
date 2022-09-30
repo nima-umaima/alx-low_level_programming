26f135ff822490650b7a0f1e148041d9db4b93af
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-30 21:04:04 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,7 +6,7 @@
 *@argv: argument vector of pointers to strings
 * Return: always 0
 */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
