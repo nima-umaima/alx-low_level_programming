bb8adbf45643e121b020763ea3d04cda02d15e2b
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-09-30 20:22:52 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,23 @@
-#include "main.h"
+#include <stdlib.h>
 /**
- *
- *
- *
+ * create_array - creates and initializes an array of chars with char c
+ *@size: size of the array
+ *@c: char to initialize the array with
  * Return: pointer to the array of char
-char()
+ */
+char *create_array(unsigned int size, char c)
 {
+char *p;
+unsigned int i;
+if (size > 0)
+{
+p = malloc(size * sizeof(char));
+if (p == NULL)
+return (NULL);
+}
+else
+return (NULL);
+for (i = 0; i < size; i++)
+p[i] = c;
 return (p);
 }
