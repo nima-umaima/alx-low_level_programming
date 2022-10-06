08621a80b3415f3bcfd417ae2effe60a57ef992f
nima-umaima <naimanysh@gmail.com>
2022-10-06 05:30:16 -0700

    1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatinates two strings
+ *@s1: string to concatinate to
+ *@s2: string to concatinate
+ * Return: pointed to allocated space
+ */
+char()
+{
+}
