0b1a9f7aedd6b7f99ba4ccf21d3e3bec5a793fad
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-12 20:39:28 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
 * struct dog - info on dog.
 *@name: dog's name.
@@ -12,9 +12,10 @@ char *name;
 char age;
 char *owner;
 };
+typedef struct dog dog_t;
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif 
