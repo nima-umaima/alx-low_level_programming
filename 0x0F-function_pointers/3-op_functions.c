f1e23e0441a8d556f5ed4fbfe742e25e4ec94848
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-13 16:42:09 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 /**
 * op_add - adds a and b
-*@a: first element
-@b: second element
-@ Return: sum of a  and b
+*@a: first number
+*@b: second number
+* Return: sum of a  and b
 */
 int op_add(int a, int b)
 {
 return (a + b);
 }
 /**
-* op_sub - subtract b from a
-*@a: first element
-*@b: second element
-*@ Return: difference of a and b
+* op_sub - subtracts b from a
+*@a: first number
+*@b: second number
+* Return: difference of a and b
 */
 int op_sub(int a, int b)
 {
@@ -23,9 +23,9 @@ return (a - b);
 }
 /**
 * op_mul - multiplies a and b
-*@a: first element
-*@b: second element
-*@ Return: multiplication of a and b
+*@a: first number
+*@b: second number
+* Return: multiplication of a and b
 */
 int op_mul(int a, int b)
 {
@@ -33,9 +33,9 @@ return (a * b);
 }
 /**
 * op_div - divides a by b
-*@a: first element
-*@b: second element
-*@ Return: integer division of a by b
+*@a: first number
+*@b: second number
+* Return: integer division of a by b
 */
 int op_div(int a, int b)
 {
@@ -47,10 +47,10 @@ return (100);
 return (a / b);
 }
 /**
-* op_div - dreminder of a by b
-*@a: first element
-*@b: second element
-*@ Return: remainder of the division of a by b
+* op_mod - find the remainder of the division of a by b
+*@a: first number
+*@b: second number
+* Return: remainder of the division of a by b
 */
 int op_mod(int a, int b)
 {
