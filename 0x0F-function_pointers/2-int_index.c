4813a9f37ef981cbe9a31645ecc95fc353e21a1e
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-13 15:20:37 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,21 @@
-#
+#include "function_pointers.h"
+/**
+* int_index - searches for an integer.
+*@array: array to search
+*@size: size of array
+*@cmp: function pointer
+* Return: index of the first element or -1
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array && cmp)
+{
+  if (size <= 0)
+    return (-1)
+    for (i = 0; i < size; i++)
+      if(cmp(array[i]))
+        return (i);
+}
+return (-1);
+}
