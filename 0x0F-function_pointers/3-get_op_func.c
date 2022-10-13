9ea4666aad5c346814ddca1a3c6f92f005e97107
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-13 15:39:28 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,1 +1,11 @@
-#
+#include "3-calc.h"
+#iclude <string.h>
+/**
+* get_op_func - selects the correct operation
+* to perform
+*@s: operator passed by arg
+* Return: pointer to the function
+*/
+int (*get_op_func(char *s))(int, int);
+{
+}
