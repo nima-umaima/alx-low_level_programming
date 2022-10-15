ac1ca8822bc8a64297b5a8bb51157daad1ccee74
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-15 19:09:49 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,6 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
-/**
-* struct print - print type with corresponding print function
-*@t: print type
-*@f: print function
-*/
 struct type
 {
 char *character;
