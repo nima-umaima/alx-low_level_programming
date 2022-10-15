8db5e58011c8ef2b34d289b8e1c99d1071bb7ea7
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-15 19:01:21 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,16 @@
-#ifndef VARIADIC_FUNCTIONS_H_
-#define VARIADIC_FUNCTIONS_H_
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+#include <stdarg.h>
+/**
+* struct print - print type with corresponding print function
+*@t: print type
+*@f: print function
+*/
+typedef struct print
+{
+char *t;
+void (*f)(va_list);
+} print_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
