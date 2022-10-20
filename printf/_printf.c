93f0f65c1df610d2281e2a09b367300753a1e91c
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-20 11:56:50 +0300

    Update _printf.c

--- printf/_printf.c
+++ printf/_printf.c
@@ -26,7 +26,7 @@ va_list arg_list;
 if (format == NULL)
 return (-1);
 va_start(arg_list, format);
-printed_chars = parser(format, f_list, arg_list);
+printed_char = parser(format, f_list, arg_list);
 va_end(arg_list);
 return (printed_chars);
 }
