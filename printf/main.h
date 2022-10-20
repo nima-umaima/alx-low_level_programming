b3c51679a6c87f23795b5a4c99b4680f76772615
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-20 11:24:49 +0300

    Update main.h

--- printf/main.h
+++ printf/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <limits.h>
 #include <stdlib.h>
-#include <sdtarg.h>
+#include <stdarg.h>
 #include <unistd.h>
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
