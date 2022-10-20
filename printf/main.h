e57b7578473744b6ab063dd233c54dcbb1b7be78
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-20 11:39:43 +0300

    Update main.h

--- printf/main.h
+++ printf/main.h
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+struct convert
+{
+char *sym;
+int (*f)(va_list);
+};
+typedef struct convert conver_t;
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 #endif
