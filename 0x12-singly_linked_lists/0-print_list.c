a120c54089e2fe13878383713974e600058c4431
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-24 19:51:46 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,13 +7,13 @@
 */
 size_t print_list(const list_t *h)
 {
-  const list_t *tmp;
-  unsigned int i;
-  tmp = h;
-  for (i = 0; tmp; i++)
-  {
-    printf("[%u] %s\n", tmp->len, tmp->str);
-    tmp = tmp->next;
-  }
-  return (i);
+const list_t *tmp;
+unsigned int i;
+tmp = h;
+for (i = 0; tmp; i++)
+{
+printf("[%u] %s\n", tmp->len, tmp->str);
+tmp = tmp->next;
+}
+return (i);
 }
