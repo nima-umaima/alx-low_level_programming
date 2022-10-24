1b38889c1d7f9e643190a9fae228a0c35967a3d9
Nima <49175722+nima-umaima@users.noreply.github.com>
2022-10-24 19:38:11 +0300

    Update 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include ".h"
+/**
+* print_list - prints all the elements of a list_t list
+*@h: pointer to the start of the list
+* Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+const list_t *tmp;
+unsigned int i;
+tmp = h;
+for (i = 0; tmp; i++)
+{
+printf("[%u] %s\n", tmp->len, tmp->str);
+tmp = tmp->next;
+}
+return (i);
+}
